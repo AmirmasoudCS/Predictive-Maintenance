946e1003776bd980eebc85ef6d4207f9baa68e48
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-16 21:17:10 +0330

    Adding the per model paths inside the configuration files for paths

--- config/paths.py
+++ config/paths.py
@@ -16,9 +16,22 @@ CSV_DATASET = DATASET / "ai4i2020.csv"
 PLOTS = ASSETS / "plots"
 PROCESSED = DATASET / "processed"
 
+LOGISTIC_REGRESSION = RESULTS / "logistic_regression"
+RANDOM_FOREST = RESULTS / "random_forest"
+XGBOOST = RESULTS / "xgboost"
+
 # Depth 3
 
 X_TRAIN = PROCESSED / "x_train.csv"
 X_TEST = PROCESSED / "x_test.csv"
 Y_TRAIN = PROCESSED / "y_train.csv"
 Y_TEST = PROCESSED / "y_test.csv"
+
+LOGISTIC_REGRESSION_METRICS = LOGISTIC_REGRESSION / "metrics.csv"
+LOGISTIC_REGRESSION_MODEL = LOGISTIC_REGRESSION / "model.pkl"
+
+RANDOM_FOREST_METRICS = RANDOM_FOREST / "metrics.csv"
+RANDOM_FOREST_MODEL = RANDOM_FOREST / "model.pkl"
+
+XGBOOST_METRICS = XGBOOST / "metrics.csv"
+XGBOOST_MODEL = XGBOOST / "model.pkl"
